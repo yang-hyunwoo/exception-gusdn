03320931e088fc45a1c2440dc5ddcf19fbdfd545
cohouseol <cohouseol@gmail.com>
2022-08-13 23:56:55 +0900

    exception
    강의를 보면서 exception 처리 및 response을 형식적으로 줄수 있는 방법을 배웠따.

---
+++ src/main/java/com/example/exceptiongusdn/ExceptionGusdnApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptiongusdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionGusdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionGusdnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exceptiongusdn/ExceptionGusdnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exceptiongusdn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionGusdnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
